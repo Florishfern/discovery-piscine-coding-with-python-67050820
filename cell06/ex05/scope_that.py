9cac50e7bb0eef6a178fc0d63d457416aec64d9c
Florishfern <116740161+Florishfern@users.noreply.github.com>
2025-09-18 09:41:34 +0700

    Add files via upload

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def add_one(x):
+    return x + 1
+
+num = 5
+print("Before calling add_one:", num)
+print("Result of add_one:", add_one(num))
+print("After calling add_one:", num)
